d46d8d8c20639680eb937547b85c65d608d1fcee
RkPranto <rezwan2525@gmail.com>
2021-10-07 01:59:44 +0600

    constant time complexity

---
+++ Time-Complexity/ConstantTime.java
@@ -0,0 +1,9 @@
+public class ConstantTime {
+    public static void main(String[] args) {
+        int a , b , ans ;
+        a = 10;
+        b = 20;
+        ans = (a * b) + (a + b);
+        System.out.println(ans);
+    }
+}
